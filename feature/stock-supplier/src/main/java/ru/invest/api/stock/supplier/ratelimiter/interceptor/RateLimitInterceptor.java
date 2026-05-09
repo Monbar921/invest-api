c4517437a7f0ecdcde417eedb7a4b8dcf7310acd
d.kornilovich <d.kornilovich@s7.ru>
2026-05-09 16:49:37 +0700

    Add aop

--- feature/stock-supplier/src/main/java/ru/invest/api/stock/supplier/ratelimiter/interceptor/RateLimitInterceptor.java
+++ feature/stock-supplier/src/main/java/ru/invest/api/stock/supplier/ratelimiter/interceptor/RateLimitInterceptor.java
@@ -6,6 +6,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+import ru.invest.api.stock.supplier.ratelimiter.service.RateLimiterService;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -25,7 +26,7 @@ public class RateLimitInterceptor {
         final String methodName = joinPoint.getSignature().getName();
         final Object[] args = joinPoint.getArgs();
 
-        log.debug("Intercepted gRPC call: {} with args: {}", methodName, args);
+        log.info("Intercepted gRPC call: {} with args: {}", methodName, args);
 
         final CompletableFuture<Object> future = rateLimiterService.executeWithRateLimit(() -> {
             try {
@@ -37,10 +38,10 @@ public class RateLimitInterceptor {
 
         try {
             return future.get(30, TimeUnit.SECONDS);
-        } catch (TimeoutException e) {
+        } catch (final TimeoutException e) {
             log.error("Rate limited request timeout for method: {}", methodName);
             throw new RuntimeException("Request timed out due to rate limiting", e);
-        } catch (ExecutionException e) {
+        } catch (final ExecutionException e) {
             throw e.getCause();
         }
     }
