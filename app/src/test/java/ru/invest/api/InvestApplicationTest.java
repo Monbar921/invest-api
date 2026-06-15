f8c1b542f154de3a379210d3f1bca67b013b3796
Monbar921 <yqemahaty@gmail.com>
2026-06-15 20:00:43 +0700

    Repair tests

--- app/src/test/java/ru/invest/api/InvestApplicationTest.java
+++ app/src/test/java/ru/invest/api/InvestApplicationTest.java
@@ -3,10 +3,15 @@ package ru.invest.api;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import ru.invest.api.budget.org.supplier.client.feign.BudgetOrgClient;
+import ru.invest.api.cb.rf.supplier.client.feign.CbRfClient;
 import ru.invest.api.common.model.BondModel;
 import ru.invest.api.dto.bond.BondDto;
 import ru.invest.api.starter.client.InvestApiBondClient;
 import ru.invest.api.tinkoff.supplier.usecase.TinkoffBondUseCase;
+import ru.tinkoff.piapi.contract.v1.InstrumentsServiceGrpc;
+import ru.tinkoff.piapi.contract.v1.MarketDataServiceGrpc;
 
 import java.util.List;
 
@@ -14,14 +19,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class InvestApplicationTest extends AbstractInvestApplicationTest {
-//    @MockitoBean
-//    private BudgetOrgClient budgetOrgClient;
-//    @MockitoBean
-//    private CbRfClient cbRfClient;
-//    @MockitoBean
-//    private InstrumentsServiceGrpc.InstrumentsServiceBlockingStub instrumentsServiceBlockingStub;
-//    @MockitoBean
-//    private MarketDataServiceGrpc.MarketDataServiceBlockingStub marketDataServiceBlockingStub;
+    @MockitoBean
+    private BudgetOrgClient budgetOrgClient;
+    @MockitoBean
+    private CbRfClient cbRfClient;
+    @MockitoBean
+    private InstrumentsServiceGrpc.InstrumentsServiceBlockingStub instrumentsServiceBlockingStub;
+    @MockitoBean
+    private MarketDataServiceGrpc.MarketDataServiceBlockingStub marketDataServiceBlockingStub;
 
     @Autowired
     private TinkoffBondUseCase tinkoffBondUseCase;
