97602a80179a8a62625af66c4bc1d090baf84baa
Monbar921 <yqemahaty@gmail.com>
2026-09-17 00:02:50 +0700

    Start writing test

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
-FROM eclipse-temurin:17-jre-alpine
+FROM eclipse-temurin:25-jre-alpine
+
+# Корневой сертификат Минцифры: им подписан сертификат invest-public-api.tinkoff.ru
+COPY certs/russian_trusted_root_ca.pem /tmp/russian_trusted_root_ca.pem
+RUN keytool -importcert -noprompt -cacerts -storepass changeit \
+      -alias russian_trusted_root_ca -file /tmp/russian_trusted_root_ca.pem \
+    && rm /tmp/russian_trusted_root_ca.pem
 
 RUN addgroup -S appgroup && adduser -S appuser -G appgroup
 WORKDIR /app
--- feature/ui-service/src/main/java/ru/invest/api/ui/service/controller/BondMaintenanceController.java
+++ feature/ui-service/src/main/java/ru/invest/api/ui/service/controller/BondMaintenanceController.java
@@ -3,13 +3,15 @@ package ru.invest.api.ui.service.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.invest.api.common.mapper.AuditMapper;
 import ru.invest.api.common.usecase.BondSyncUseCase;
 
 import static org.springframework.http.HttpHeaders.FROM;
 
-@RestController("/internal/rest/maintenance")
+@RestController
+@RequestMapping("/internal/rest/maintenance")
 @RequiredArgsConstructor
 public class BondMaintenanceController {
     private final BondSyncUseCase bondSyncUseCase;
